f827d36a6e8803abacd394d245583df9b1d571f3
Anjali Sinha <anjalisinha800001@gmail.com>
2023-06-23 19:09:18 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,486 @@
+
+
+var quiz = { "JS" : [
+	{
+		"id" : 1,
+		"question" : "Which one of the below is not divide and conquer approach?",
+		"options" : [
+			{"a": "Insertion Sort", 
+			 "b":" Merge Sort", 
+			 "c":"Shell Sort", 
+			 "d":"Heap Sort"}
+			],
+		"answer":"Merge Sort",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 2,
+		"question" : "Postfix expression is just a reverse of prefix expression.",
+		"options" : [
+			{"a": "True", 
+			 "b":"False", 
+			 }
+			],
+		"answer":"False",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 3,
+		"question" : " Minimum number of moves required to solve a Tower of Hanoi puzzle is",
+		"options" : [
+			{"a": " 2n^2", 
+			 "b":"2n-1", 
+			 "c":"2^n-1;"}
+			],
+		"answer":"2^n-1;",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 4,
+		"question" : "Minimum number of spanning tree in a connected graph is",
+		"options" : [
+			{"a": "n", 
+			 "b":"n^n-1",
+			 "c":"1",
+			 "d":"0"
+			}
+			],
+		"answer":"1",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 5,
+		"question" : " Which of the following is example of in-place algorithm?",
+		"options" : [
+			{"a": "Bubble Sort", 
+			 "b":"Merge Sort",
+			 "c":"Insertion Sort",
+			 "d":"All of the above",
+			}
+			],
+		"answer":"Merge Sort",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 6,
+		"question" : "The number of binary trees with 3 nodes which when traversed in post order gives the sequence A,B,C is ?",
+		"options" : [
+			{"a": "3", 
+			 "b":"4",
+			 "c":"5",
+			 "d":"6",
+			}
+			],
+		"answer":"5",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 7,
+		"question" : "node.next -> node.next.next; will make",
+		"options" : [
+			{"a": "node.next inaccessible", 
+			 "b":"node.next.next inaccessible",
+			 "c":"this node inaccessible",
+			 "d":" none of the above"
+			}
+			],
+		"answer":"node.next inaccessible",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 8,
+		"question" : "Which of the following algorithm cannot be desiged without recursion −",
+		"options" : [
+			{"a": "Tower of Hanoi", 
+			 "b":"Fibonacci Series",
+			 "c":"Tree Traversal",
+			 "d":" None of the above ",
+			}
+			],
+		"answer":" None of the above",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 9,
+		"question" : "If we choose Prim's Algorithm for uniquely weighted spanning tree instead of Kruskal's Algorithm, then",
+		"options" : [
+			{"a": "we'll get a different spanning tree.", 
+			 "b":"we'll get the same spanning tree.",
+			 "c":" spanning will have less edges.",
+			 "d":"spanning will not cover all vertices."
+			}
+			],
+		"answer":"we'll get the same spanning tree.",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 10,
+		"question" : "Aposterior analysis are more accurate than apriori analysis because −",
+		"options" : [
+			{"a": "it contains the real data.", 
+			 "b":" it assumes all other factors to be dynamic.",
+			 "c":"it assumes all other factors to be constant.",
+			 "d":" it is a result of reverse-engineering.",
+			}
+			],
+		"answer":" it assumes all other factors to be dynamic.",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 11,
+		"question" : "Which of the following points is/are true about Linked List data structure when it is compared with array?",  
+		"options" : [
+			{"a": "Arrays have better cache locality that can make them better in terms of performance.", 
+			 "b":"It is easy to insert and delete elements in Linked List",
+			 "c":"Random access is not allowed in a typical implementation of Linked Lists",
+			 "d":"The size of array has to be pre-decided, linked lists can change their size any time.",
+			 "e":"All of the above"
+			}
+			],
+		"answer":"All of the above",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 12,
+		"question" : "Which of the following sorting algorithms can be used to sort a random linked list with minimum time complexity?",
+		"options" : [
+			{"a": "Insertion Sort", 
+			 "b":"Quick Sort",
+			 "c":"Heap Sort",
+			 "d":"Merge Sort"
+			}
+			],
+		"answer":"Merge sort",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 13,
+		"question" : "In linked list each node contains a minimum of two fields. One field is data field to store the data second field is?",
+		"options" : [
+			{"a": " Pointer to character", 
+			 "b":"Pointer to integer",
+			 "c":"Pointer to node",
+			 "d":"Node"
+			}
+			],
+		"answer":"Pointer to node",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 14,
+		"question" : "What would be the asymptotic time complexity to add a node at the end of singly linked list, if the pointer is initially pointing to the head of the list?",
+		"options" : [
+			{"a": "O(1)", 
+			 "b":"O(n)",
+			 "c":"θ(n)",
+			 "d":"θ(1)"
+			}
+			],
+		"answer":"θ(n)",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 15,
+		"question" : "What would be the asymptotic time complexity to insert an element at the front of the linked list (head is known)?",
+		"options" : [
+			{"a": "O(1)", 
+			 "b":"O(n)",
+			 "c":"O(n^2)",
+			 "d":"O(n^3)"
+			}
+			],
+		"answer":"O(1)",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 16,
+		"question" : "What would be the asymptotic time complexity to find an element in the linked list?",
+		"options" : [
+			{"a": "O(1)", 
+			 "b":"O(n)",
+			 "c":"O(n^2)",
+			 "d":"O(n^4)"
+			}
+			],
+		"answer":"O(n)",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 17,
+		"question" : "What would be the asymptotic time complexity to insert an element at the second position in the linked list?",
+		"options" : [
+			{"a": "O(1)", 
+			 "b":"O(n)",
+			 "c":"O(n^2)",
+			 "d":"O(n^3)"
+			}
+			],
+		"answer":"O(1)",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 18,
+		"question" : "The concatenation of two lists can be performed in O(1) time. Which of the following variation of the linked list can be used?",
+		"options" : [
+			{"a": "Singly linked list", 
+			 "b":"Doubly linked list",
+			 "c":"Circular doubly linked list",
+			 "d":"Array implementation of list"
+
+			}
+			],
+		"answer":"Circular doubly linked list",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 19,
+		"question" : "Express -15 as a 6-bit signed binary number.",
+		"options" : [
+			{"a": " 001111", 
+			 "b":"101111",
+			 "c":"101110",
+			 "d":"001110"
+
+			}
+			],
+		"answer":"101111",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 20,
+		"question" : "Which is the predefined method available in Java to convert decimal to binary numbers?",
+		"options" : [
+			{"a": "toBinaryInteger(int)", 
+			 "b":"toBinaryValue(int)",
+			 "c":"toBinaryNumber(int)",
+			 "d":"toBinaryString(int)"
+			}
+			],
+		"answer":"toBinaryString(int)",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 21,
+		"question" : "What is the time complexity for converting decimal to binary numbers?",
+		"options" : [
+			{"a": "O(1)", 
+			 "b":"O(n)",
+			 "c":"O(logn)",
+			 "d":"O(nlogn)"
+			}
+			],
+		"answer":"O(logn)",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 22,
+		"question" : "To implement a stack using queue(with only enqueue and dequeue operations), how many queues will you need?",
+		"options" : [
+			{"a": "1", 
+			 "b":"2",
+			 "c":"3",
+			 "d":"4"
+			}
+			],
+		"answer":"2",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 23,
+		"question" : "Process of inserting an element in stack is called ____________",
+		"options" : [
+			{"a": "Create", 
+			 "b":"Push",
+			 "c":"Evalution",
+			 "d":"Pop"
+			}
+			],
+		"answer":"Push",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 24,
+		"question" : " Process of removing an element from stack is called __________",
+		"options" : [
+			{"a": "Create", 
+			 "b":"Push",
+			 "c":"Evalution",
+			 "d":"Pop"
+			}
+			],
+		"answer":"Pop",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 25,
+		"question" : "In a stack, if a user tries to remove an element from an empty stack it is called _________",
+		"options" : [
+			{"a": "Underflow", 
+			 "b":"Empty collection",
+			 "c":"Overflow",
+			 "d":"Garbage Collection"
+			}
+			],
+		"answer":"Underflow",
+		"score":0,
+		"status": ""
+	}
+	]
+}
+
+
+
+var quizApp = function() {
+
+	this.score = 0;		
+	this.qno = 1;
+	this.currentque = 0;
+	var totalque = quiz.JS.length;
+
+	
+	this.displayQuiz = function(cque) {
+		this.currentque = cque;
+		if(this.currentque <  totalque) {
+			$("#tque").html(totalque);
+			$("#previous").attr("disabled", false);
+			$("#next").attr("disabled", false);
+			$("#qid").html(quiz.JS[this.currentque].id + '.');
+	
+			
+			$("#question").html(quiz.JS[this.currentque].question);	
+			 $("#question-options").html("");
+			for (var key in quiz.JS[this.currentque].options[0]) {
+			  if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+		
+				$("#question-options").append(
+					"<div class='form-check option-block'>" +
+					"<label class='form-check-label'>" +
+							  "<input type='radio' class='form-check-input' name='option'   id='q"+key+"' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+								  quiz.JS[this.currentque].options[0][key] +
+							 "</span></label>"
+				);
+			  }
+			}
+		}
+		if(this.currentque <= 0) {
+			$("#previous").attr("disabled", true);	
+		}
+		if(this.currentque >= totalque) {
+				$('#next').attr('disabled', true);
+				for(var i = 0; i < totalque; i++) {
+					this.score = this.score + quiz.JS[i].score;
+				}
+			return this.showResult(this.score);	
+		}
+	}
+	
+	this.showResult = function(scr) {
+		$("#result").addClass('result');
+		$("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr  + '/' + totalque + "</h1>");
+		for(var j = 0; j < totalque; j++) {
+			var res;
+			if(quiz.JS[j].score == 0) {
+					res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+			} else {
+				res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+			}
+			$("#result").append(
+			'<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+			'<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+			'<div class="last-row"><b>Score:</b> &nbsp;' + res +
+			
+			'</div>' 
+			
+			);
+			
+		}
+	}
+	
+	this.checkAnswer = function(option) {
+		var answer = quiz.JS[this.currentque].answer;
+		option = option.replace(/\</g,"&lt;")   //for <
+		option = option.replace(/\>/g,"&gt;")   //for >
+		option = option.replace(/"/g, "&quot;")
+
+		if(option ==  quiz.JS[this.currentque].answer) {
+			if(quiz.JS[this.currentque].score == "") {
+				quiz.JS[this.currentque].score = 1;
+				quiz.JS[this.currentque].status = "correct";
+		}
+		} else {
+			quiz.JS[this.currentque].status = "wrong";
+		}
+		
+	}	
+	 
+	this.changeQuestion = function(cque) {
+			this.currentque = this.currentque + cque;
+			this.displayQuiz(this.currentque);	
+			
+	}
+	
+}
+
+
+var jsq = new quizApp();
+
+var selectedopt;
+	$(document).ready(function() {
+			jsq.displayQuiz(0);		
+		
+	$('#question-options').on('change', 'input[type=radio][name=option]', function(e) {
+
+			//var radio = $(this).find('input:radio');
+			$(this).prop("checked", true);
+				selectedopt = $(this).val();
+		});
+		
+			
+			 
+	});
+
+	
+	
+	
+	$('#next').click(function(e) {
+			e.preventDefault();
+			if(selectedopt) {
+				jsq.checkAnswer(selectedopt);
+			}
+			jsq.changeQuestion(1);
+	});	
+	
+	$('#previous').click(function(e) {
+		e.preventDefault();
+		if(selectedopt) {
+			jsq.checkAnswer(selectedopt);
+		}
+			jsq.changeQuestion(-1);
+	});	
+
+
+
